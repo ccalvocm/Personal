69ff71202c0fc3a9f57cb56af31ab31dbadc2916
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2020-11-23 11:41:30 -0300

    Orden

---
+++ Hydrology/Validar_CVE.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 14:47:17 2020
+
+@author: ccalvo
+"""
+
+def limpiar_kernel():    
+    try:
+        from IPython import get_ipython
+        get_ipython().magic('clear')
+        get_ipython().magic('reset -f')
+    except:
+        pass
+ 
+limpiar_kernel()
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+import os
+
+def parametros_distribucion(data_, distr_):
+    params = distr_.fit(data_)
+
+    return params
+
+def CVEParser(txt_):
+    CVE = dict()
+    n = 0
+    station = None
+    data = None
+    with open(txt_, 'r') as f:
+        for line in f:
+            L = line.split()
+            if len(L) == 1:
+                station = L[0]
+                data = []
+            elif len(L) > 1:
+                data.append(L)
+                n +=1
+            else:
+                pass
+            if n >= 7:
+                n = 0
+                CVE[station] = data
+            else:
+                pass
+
+    return CVE
+
+
+def CVE(dataframe, quantiles, months = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+                                        'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+                                        'Nov', 'Dec'],
+        hydroyear = True, aggregate = True):
+    '''
+    
+
+ 
+
+    Parameters
+    ----------
+    dataframe : Pandas dataframe
+        Pandas dataframe with index as datestamps and columns as stations
+    quantiles : array or list
+        List of quantiles from 0 to 1 e.g [0.85, 0.9]. 85 and 90 quantile
+    months : array or list of months in english with 3 characters e.g 'Jan'
+        List of months of the year.
+    hydroyear : boolean, default to False
+        Set to true if you wish to use the hydrologic year in the southern
+        hemisphere: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+                     'Nov', 'Dec', 'Jan', 'Feb', 'Mar']
+
+ 
+
+    Returns
+    -------
+    mdf : list of Pandas dataframes
+        returns list of dataframes showing the quantile month calculation
+        for all stations.
+
+ 
+
+    '''
+    if hydroyear:
+        months = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+                     'Nov', 'Dec', 'Jan', 'Feb', 'Mar']
+    else:
+        pass
+    
+    mdf = dict()
+    if aggregate:
+        for qtile in quantiles:
+            MonthAV = dataframe.groupby(dataframe.index.month).quantile(q = 1-qtile)
+            MonthAV['Total'] = MonthAV.mean(axis = 1)
+            MonthAV.index = pd.to_datetime(MonthAV.index, format = '%m')
+            MonthAV.index = MonthAV.index.month_name().str.slice(stop=3)
+            MonthAV = MonthAV.reindex(months)
+            mdf[str(qtile)] = MonthAV
+    else:
+        mdf = dict()
+        for col in dataframe.columns:
+            qtl_group = dict()
+            for qtile in quantiles:
+                MonthAV = dataframe[col].groupby(dataframe.index.month).quantile(q = 1-qtile)
+                # MonthAV['Total'] = MonthAV.mean(axis = 1)
+                MonthAV.index = pd.to_datetime(MonthAV.index, format = '%m')
+                MonthAV.index = MonthAV.index.month_name().str.slice(stop=3)
+                MonthAV = MonthAV.reindex(months)
+                qtl_group[str(qtile)] = MonthAV
+            mdf[col] = qtl_group
+    return mdf
+
+def main():
+  
+# Informe: 
+# DIAGNOSTICO Y CLASIFICACION DE LOS CURSOS Y CUERPOS DE AGUA SEGUN OBJETIVOS DE CALIDAD
+# CUENCA DEL RIO MAIPO
+# JULIO 2004
+# URL: https://mma.gob.cl/wp-content/uploads/2017/12/Maipo.pdf
+  
+  # inputs:
+  ruta_Git = r'C:\Users\ccalvo\Documents\GitHub'
+  ruta_Git = 'D:\GitHub'
+  ruta_Q_rellenos = ruta_Git+r'\Analisis-Oferta-Hidrica\Hidrología\Caudales\Validacion\Q_relleno_MLR_Maipo_1950-2001_outlier_correction_median.csv'
+  ruta_Q = ruta_Git+r'\Analisis-Oferta-Hidrica\Hidrología\Caudales\Validacion\cr2_Maipo_Q.xlsx'
+
+
+
+  # ruta_Q_rellenos = ruta_Git+r'\WEAP-MODFLOW-personal\Hydrology\Q_relleno_MLR_Maipo_1950-2001.csv'
+  probabilidades_excedencia = [.05, .1, .2, .5, .85, .95]
+  
+  estaciones_date = {'05710001-K': ['1950-01-01', '2001-12-31'],
+       '05701001-0': ['1979-01-01', '2001-12-31'],
+       '05701002-9': ['1962-01-01', '1993-12-31'],
+       '05702001-6': ['1950-01-01', '2001-12-31'],
+       '05704002-5': ['1950-01-01', '2001-12-31'],
+       '05705001-2': ['1977-01-01', '2001-12-31'],
+       '05706001-8': ['1977-01-01', '2001-12-31'],
+       '05707002-1': ['1950-01-01', '2001-12-31'],
+       '05721001-K': ['1986-01-01', '2001-12-31'],
+       '05722001-5': ['1952-01-01', '2001-12-31'],
+       '05722002-3': ['1950-01-01', '2001-12-31'],
+       '05716001-2': ['1981-01-01', '2001-12-31'],
+       '05735001-6': ['1980-01-01', '2001-12-31'],
+       '05737002-5': ['1959-01-01', '2001-12-31'],
+       '05741001-9': ['1950-01-01', '2001-12-31'],
+       '05746001-6': ['1986-01-01', '2001-12-31'],
+       '05748001-7': ['1979-01-01', '2001-12-31']}
+  
+  ruta = ruta_Git+r'\Analisis-Oferta-Hidrica\Hidrología\Caudales\Validacion'
+  os.chdir(ruta)
+
+  ruta_datos = 'Validacion_Maipo_DGA_2004.txt'
+  
+  cve = CVEParser(ruta_datos)
+  Q_relleno = pd.read_csv(ruta_Q_rellenos, index_col = 0)[estaciones_date.keys()]
+  Q_relleno.index = pd.to_datetime(Q_relleno.index)
+  inicio = pd.to_datetime('1950-01-01',format='%Y-%m-%d')
+  fin = pd.to_datetime('2001-12-31',format='%Y-%m-%d')
+  Q_relleno = pd.DataFrame(Q_relleno[Q_relleno.index <= fin ],  index = pd.date_range(inicio, fin, freq='D', closed='right'))
+  
+  Q = pd.read_excel(ruta_Q, col_index = 0)
+  Q = Q[estaciones_date.keys()]
+  
+  Q.index = pd.to_datetime(Q.index)
+  inicio = pd.to_datetime('1950-01-01',format='%Y-%m-%d')
+  fin = pd.to_datetime('2001-12-31',format='%Y-%m-%d')
+  Q = pd.DataFrame(Q[(Q.index <= fin) &  (Q.index >= inicio)],  index = pd.date_range(inicio, fin, freq='D', closed='right'))
+  Q = Q.dropna()
+    
+   # crear lista de pbb de excedencia de meses por estaciones
+
+  caudales_pbb_mes = {x:'' for x in estaciones_date.keys()}
+  caudales_pbb_mes_sinr = {x:'' for x in estaciones_date.keys()}
+
+
+  # calcular CVE
+
+  pbb_mensuales = pd.DataFrame(columns=[probabilidades_excedencia], index = [0,1,2,3,4,5,6,7,8,9,10,11])
+  pbb_mensuales_sinr = pd.DataFrame(columns=[probabilidades_excedencia], index = [0,1,2,3,4,5,6,7,8,9,10,11])
+
+
+  r = -1
+  c = 0
+  plt.close("all")
+  fig, axes = plt.subplots(6,3)
+  
+
+  # iterar sobre estaciones
+  for i,estacion in enumerate(estaciones_date):
+                  
+    cve_informe = pd.DataFrame(cve[estacion]).iloc[:-1,:].transpose().astype(float)
+    
+    for index, col in enumerate(probabilidades_excedencia):
+        
+        fechas = pd.to_datetime(estaciones_date[estacion])
+    
+        CVE_rellenada = CVE(pd.DataFrame(Q_relleno[estacion].loc[(Q_relleno.index <= fechas[-1]) & (Q_relleno.index >= fechas[0])], columns = [estacion], index = Q_relleno.index), probabilidades_excedencia)[str(probabilidades_excedencia[index])][estacion]
+
+        CVE_sinr = CVE(pd.DataFrame(Q[estacion].loc[(Q.index <= fechas[-1]) & (Q.index >= fechas[0])], columns = [estacion], index = Q.index), probabilidades_excedencia)[str(probabilidades_excedencia[index])][estacion]
+       
+        pbb_mensuales.loc[pbb_mensuales.index, col] =  CVE_rellenada.to_list()
+        
+        pbb_mensuales_sinr.loc[pbb_mensuales_sinr.index, col] =  CVE_sinr.to_list()
+
+        
+    caudales_pbb_mes[estacion] = pbb_mensuales
+    caudales_pbb_mes_sinr[estacion] = pbb_mensuales_sinr
+    
+#Graficar
+
+    if (i+1)%3 == 0:
+        r += 1
+        c = 0
+    
+    axis = axes[r,c]
+    # cve_informe.plot(color = 'r', ax = axis, legend=False, linewidth = 3)
+    caudales_pbb_mes_sinr[estacion].plot(ax = axis, color = 'b', legend=False, linewidth = 3)
+    caudales_pbb_mes[estacion].plot(ax = axis, color = 'b', legend=False, linewidth = 3)
+
+    axis.set_xticklabels(['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+                     'Nov', 'Dec', 'Jan', 'Feb', 'Mar'])
+    axis.set_ylabel('Q $m^3/s$')
+    axis.set_title('Estación '+estacion)
+
+    c += 1
+        
+  axis.legend(['Informe','Rellenada'],bbox_to_anchor=(1.05, 1.05), loc='upper left')    
+
+if __name__ == '__main__':
+    main()
+        
+        
+
+
+
+
+
+
